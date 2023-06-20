708bb1fb284e91c64e7aded8901d495b01560d3c
GoldenStar1997 <golden.starr1997@gmail.com>
2023-06-20 08:41:42 +0200

    6.20

--- util/config.js
+++ util/config.js
@@ -1,5 +1,5 @@
-const clientURL = 'http://localhost:3000/';
-const InviteURL = 'http://localhost:3000/invite';
+const clientURL = 'http://localhost:8483/';
+const InviteURL = 'http://localhost:8483/invite';
 
 module.exports = {
   clientURL,
